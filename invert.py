178ca8fa8dcd9ceed660dfb08b97162755cbcb3d
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-02 15:58:45 -0400

    Update invert.py

--- invert.py
+++ invert.py
@@ -12,15 +12,23 @@ def get_determinant(A):
 
 def get_inverse(A):
     if len(A) == 2:
-        inv_A = [[A[1][1]/(get_determinant(A)),-1*A[0][1]/(get_determinant(A))],
-        [-1*A[1][0]/(get_determinant(A)),A[0][0]/(get_determinant(A))]]
+        inv_A = [
+            [ A[1][1], -A[0][1]],
+            [-A[1][0],  A[0][0]]
+        ]
     
     if len(A) == 3:
-        inv_A = [[((A[1][1]*A[2][2])-(A[1][2]*A[2][1]))/get_determinant(A), ((A[0][2]*A[2][1])-(A[0][1]*A[2][2]))/get_determinant(A), ((A[0][1]*A[1][2])-(A[0][2]*A[1][1]))/get_determinant(A)],
-        [((A[1][2]*A[2][0])-(A[1][0]*A[2][2]))/get_determinant(A), ((A[0][0]*A[2][2])-(A[0][2]*A[2][0]))/get_determinant(A), ((A[0][2]*A[1][0])-(A[0][0]*A[1][2]))/get_determinant(A)],
-        [((A[1][0]*A[2][1])-(A[1][1]*A[2][0]))/get_determinant(A), ((A[0][1]*A[2][0])-(A[0][0]*A[2][1]))/get_determinant(A), ((A[0][0]*A[1][1])-(A[0][1]*A[1][0]))/get_determinant(A)]]
+        inv_A = [
+            [A[1][1]*A[2][2] - A[1][2]*A[2][1], A[0][2]*A[2][1] - A[0][1]*A[2][2], A[0][1]*A[1][2] - A[0][2]*A[1][1]],
+            [A[1][2]*A[2][0] - A[1][0]*A[2][2], A[0][0]*A[2][2] - A[0][2]*A[2][0], A[0][2]*A[1][0] - A[0][0]*A[1][2]],
+            [A[1][0]*A[2][1] - A[1][1]*A[2][0], A[0][1]*A[2][0] - A[0][0]*A[2][1], A[0][0]*A[1][1] - A[0][1]*A[1][0]]
+        ]
     
-    return inv_A
+    return scalar_mul(1 / get_determinant(A), inv_A)
+
+
+def scalar_mul(scalar, matrix):
+    return [[scalar * n for n in m] for m in matrix]
 
 
 def transpose(A):
